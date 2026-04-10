12aa3037ec5e1ec935f8f895facf8cfc58a063a3
gamsung0388 <ryan9320@gmail.com>
2026-04-11 02:32:29 +0900

    개발, 로컬 세팅

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [vue()],
+    base: process.env.VITE_BASE,
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
     }
-  },
-  base: '/Imbr_InsangTest/', // 🔥 레포 이름
+  }
 })
